8b762b8d4f56225be135b1a4d7138f67cc78df48
Ljojua15 <jojualasha15@gmail.com>
2024-03-22 16:03:33 +0400

    last-changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,122 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription, take} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Timer';
+export class AppComponent implements  OnInit, OnDestroy {
+  progress: number = 0;
+  noOfFiles: number = 0;
+  completed: boolean = false;
+  changeTime: number = this.noOfFiles * 60;
+  subscription: Subscription | undefined;
+
+  minutes:number = 20;
+  seconds:number = 10;
+  timer :any
+  date = new Date();
+
+  stopBtn: boolean = false;
+  startBtm: boolean = false;
+  title: any;
+
+
+
+  ngOnInit(): void {
+    this.updateProgress();
+    this.updateTimer()
+    this.start()
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+
+    }
+  }
+
+  // incr(type: 'M' | 'S'){
+  //   if(type === 'M'){
+  //     if(this.minutes >= 59) return;
+  //     this.minutes += 1;
+  //   }else {
+  //     if(this.seconds >=59) return;
+  //     this.seconds += 1
+  //   }
+  // }
+  // decr(type: 'M' | 'S'){
+  //   if(type === 'M'){
+  //     if(this.minutes <= 0) return;
+  //     this.minutes -= 1;
+  //   }else {
+  //     if(this.seconds <=0) return;
+  //     this.seconds -= 1
+  //   }
+  // }
+
+
+  updateTimer(){
+    this.date.setMinutes(this.minutes)
+    this.date.setSeconds(this.seconds)
+    this.date.setMilliseconds(0)
+
+    const time = this.date.getTime()
+    this.date.setTime(time - 1000)
+
+    this.minutes = this.date.getMinutes();
+    this.seconds = this.date.getSeconds()
+
+    if (this.date.getMinutes() === 0 &&
+      this.date.getSeconds() === 0){
+      clearInterval(this.timer);
+      setTimeout(() =>{
+        this.stop()
+      }, 5000)
+
+    }
+  }
+
+  stop(){
+    clearInterval(this.timer)
+    this.startBtm = true
+  }
+
+  reset(){
+    this.minutes = 0;
+    this.seconds = 0;
+    this.stop()
+  }
+
+
+  start(){
+    if(this.minutes > 0 || this.seconds > 0){
+      this.stopBtn = true;
+      this.updateTimer()
+
+      if (this.seconds > 0 ) {
+        this.timer = setInterval(() => {
+          this.updateTimer();
+        }, 1000)
+      }
+    }
+  }
+
+
+
+
+  updateProgress() {
+    this.completed = false;
+    const n = 100 / this.changeTime;
+    this.subscription = interval(1000)
+      .pipe(
+        take(this.changeTime + 1)
+      )
+      .subscribe(() => {
+        this.progress += n;
+        console.log(Math.round(this.progress));
+
+      });
+  }
 }
--- src/app/circle-timer/circle-timer.component.ts
+++ src/app/circle-timer/circle-timer.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Input, Renderer2, SimpleChanges, ViewChild,} from '@angular/core';
 
 @Component({
   selector: 'app-circle-timer',
   templateUrl: './circle-timer.component.html',
   styleUrls: ['./circle-timer.component.scss']
 })
-export class CircleTimerComponent {
+export class CircleTimerComponent  {
+
+  @Input() progress: number = 1000;
+  @ViewChild('swiperColor') swiperColor!: ElementRef;
+  @ViewChild('colorCircle') colorCircle!: ElementRef;
+
+  test = false
+  firstTimer:number =20
+  secondTimer:number =10
+  thirdTimer:number =30
+
+  mainTimer !: number
+  colorChange : string = '#f87070'
+
+  constructor(private renderer: Renderer2) {}
+  ngAfterViewInit() {
+
+
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+
+  loadData(arg:number, type:string) {
+    const colorCircle = this.colorCircle.nativeElement
+    this.renderer.setStyle(colorCircle, 'background-color', 'blue');
+
+
+
+    if(type === 'first') {
+      this.colorChange ='blue';
+      this.renderer.setStyle(colorCircle, 'background-color', 'blue');
+    }else if(type === 'second') {
+      this.colorChange ='#f87070';
+      this.renderer.setStyle(colorCircle, 'background-color', '#f87070');
+    }else if(type === 'third') {
+      this.colorChange ='red';
+      this.renderer.setStyle(colorCircle, 'background-color', 'red');
+
+    }
+
+    // @ts-ignore
+    this.renderer.setStyle(colorCircle, 'background', `conic-gradient( #161932 ${this.progress}%, ${this.colorChange} 0%)`);
+    this.mainTimer = arg
+  }
+
+
+
+
+
+
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    this.moveSwiperColor(0);
+  }
+
+
+
+  moveSwiperColor(position: number) {
+    this.test =false
+    const swiperColor = this.swiperColor.nativeElement;
+    this.renderer.setStyle(swiperColor, 'transition', 'left 0.5s ease-out');
+
+    switch (position) {
+      case 1:
+        this.renderer.setStyle(swiperColor, 'left', '5px');
+        this.renderer.setStyle(swiperColor, 'right', 'auto');
+        this.loadData(this.firstTimer, 'first')
+        console.log(this.firstTimer)
+        break;
+      case 2:
+        this.renderer.setStyle(swiperColor, 'left', '33.5%');
+        this.loadData(this.secondTimer, 'second');
+        break;
+      case 3:
+        this.renderer.setStyle(swiperColor, 'left', 'auto');
+        this.renderer.setStyle(swiperColor, 'left', '65.5%');
+        this.loadData(this.thirdTimer, 'third');
+        console.log(this.thirdTimer);
+
+
+        break;
+      default:
+        this.renderer.setStyle(swiperColor, 'left', '33.5%');
+        this.renderer.setStyle(swiperColor, 'right', 'auto');
+        this.renderer.setStyle(swiperColor, 'display', 'block');
+        this.loadData(this.secondTimer, 'second');
+        break;
+
+    }
+
+
+  }
+
+
 
 }
+
