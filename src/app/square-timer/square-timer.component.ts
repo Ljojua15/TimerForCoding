4c0eed062b5d1c058636d5c303918484baca788d
Ljojua15 <jojualasha15@gmail.com>
2024-03-27 14:16:00 +0400

    squarea-tim1r

--- src/app/square-timer/square-timer.component.ts
+++ src/app/square-timer/square-timer.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SquareTimerComponent {
 
+  public hours: number = 0;
+  public minutes: number = 0;
+  public seconds: number = 0;
+
+
+
 }
