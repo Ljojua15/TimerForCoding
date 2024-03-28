0aa5963e3144a8e3702e6c120136357055a3283b
Ljojua15 <jojualasha15@gmail.com>
2024-03-28 09:50:11 +0400

    functional

--- src/app/square-timer/square-timer.component.ts
+++ src/app/square-timer/square-timer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-square-timer',
@@ -10,6 +10,22 @@ export class SquareTimerComponent {
   public hours: number = 0;
   public minutes: number = 0;
   public seconds: number = 0;
+
+  private timer : any;
+  private data = new Date();
+
+  show:boolean = true;
+  disabled:boolean = false;
+  animate: boolean = false;
+
+  @ViewChild("isAudio") isAudio!: ElementRef;
+
+  constructor() {
+  }
+
+  ngOnInit(){
+
+  }
 
 
 
