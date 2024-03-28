a388a64bf3e6c9c96219ca1d787a73dbf38cb045
Ljojua15 <jojualasha15@gmail.com>
2024-03-28 18:15:55 +0400

    functional1

--- src/app/square-timer/square-timer.component.ts
+++ src/app/square-timer/square-timer.component.ts
@@ -12,13 +12,103 @@ export class SquareTimerComponent {
   public seconds: number = 0;
 
   private timer : any;
-  private data = new Date();
+  private date = new Date();
 
   show:boolean = true;
   disabled:boolean = false;
   animate: boolean = false;
 
   @ViewChild("isAudio") isAudio!: ElementRef;
+
+
+  zrda(type:'H'| 'M' | 'S'){
+    if (type ==='H'){
+      if (this.hours >= 99) return;
+      this.hours +=1
+    } else if(type === 'M'){
+      if (this.minutes >= 59) return;
+      this.minutes = this.minutes + 1
+    } else {
+      if (this.seconds >= 59) return;
+      this.seconds += 1
+    }
+  }
+
+  kleba(type:'H'| 'M' | 'S'){
+    if (type ==='H'){
+      if (this.hours <= 0) return;
+      this.hours -=1
+    } else if(type === 'M'){
+      if (this.minutes <= 0) return;
+      this.minutes = this.minutes - 1
+    } else {
+      if (this.seconds <= 0) return;
+      this.seconds -= 1
+    }
+  }
+
+  updateTimer(){
+    this.date.setHours(this.hours)
+    this.date.setMinutes(this.minutes)
+    this.date.setSeconds(this.seconds)
+
+    this.date.setMilliseconds(0)
+
+    const time = this.date.getTime()
+
+    this.date.setTime(time -1000)
+
+    this.hours = this.date.getHours()
+    this.minutes = this.date.getMinutes()
+    this.seconds = this.date.getSeconds()
+
+    if (this.date.getHours() === 0 &&
+      this.date.getMinutes() === 0 &&
+      this.date.getSeconds() ===0
+    ){
+      clearInterval(this.timer);
+      this.isAudio.nativeElement.play();
+      this.animate = true;
+      setTimeout(() =>{
+        this.stop();
+        this.isAudio.nativeElement.load();
+
+      }, 5000)
+    }
+  }
+
+  stop(){
+    this.disabled = false;
+    this.show = true;
+    this.animate = false;
+    clearInterval(this.timer)
+    this.isAudio.nativeElement.load()
+  }
+
+  reset(){
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.stop()
+  }
+
+  start() {
+    if (this.hours > 0 || this.minutes > 0 || this.seconds > 0){
+      this.disabled = true;
+      this.show = false
+      this.updateTimer()
+
+      if (this.seconds > 0){
+        this.timer = setInterval(()=>{
+          this.updateTimer();
+        }, 1000)
+      }
+    }
+      }
+
+
+
+
 
   constructor() {
   }
